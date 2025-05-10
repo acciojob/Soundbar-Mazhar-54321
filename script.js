5bb3f17d06b3d0c36f39b8e06fb46c63149f7ee2
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-10 10:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function playSound(soundName) {
+    stopSounds(); // Stop any currently playing sounds
+    const audio = new Audio(`sounds/${soundName}.mp3`);
+	console.log(audio)
+    audio.play();
+    window.currentAudio = audio;
+}
+
+function stopSounds() {
+    if (window.currentAudio) {
+        window.currentAudio.pause();
+        window.currentAudio.currentTime = 0;
+    }
+}
