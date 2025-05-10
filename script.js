83ace71b5b2a4cce16a7881bf4166a4b67bc28e2
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-10 10:34:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //your JS code here. If required.
 function playSound(soundName) {
-    stopSounds(); // Stop any currently playing sounds
-    const audio = new Audio(`sounds/${soundName}.mp3`);
-	console.log(audio)
-    audio.play();
+   const existing = document.getElementById('dynamic-audio');
+    if (existing) existing.remove();
+    const audio = document.createElement('audio');
+    audio.id = 'dynamic-audio';
+    audio.src = `sounds/${soundName}.mp3`;
+    audio.autoplay = true;
+    document.body.appendChild(audio);
+
     window.currentAudio = audio;
 }
 
